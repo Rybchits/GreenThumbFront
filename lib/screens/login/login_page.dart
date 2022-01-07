c7f9b7a0accb8537d557ac771a721deba420e5b4
Rybchits <Grinya110@gmail.com>
2022-01-08 01:19:33 +0300

    [fix] fixed signUp link in login page

--- lib/screens/login/login_page.dart
+++ lib/screens/login/login_page.dart
@@ -59,7 +59,7 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   // Обработчик нажатия на ссылку регистрации
-  Future<void> onSignUpLinkClick() async {
+  void onSignUpLinkClick() {
     Navigator.popAndPushNamed(context, '/registration');
   }
 
@@ -140,7 +140,7 @@ class _LoginPageState extends State<LoginPage> {
       child: Text('Нет аккаунта? Зарегистрируйтесь!',
           style: TextStyle(
               fontSize: 14, color: Theme.of(context).primaryColorDark, fontWeight: FontWeight.w600)),
-      onTap: () => onSignUpLinkClick,
+      onTap: onSignUpLinkClick,
     );
 
     return GestureDetector(
